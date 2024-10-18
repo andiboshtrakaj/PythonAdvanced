f5c6ea4fa85237e8a77965aa28598fed6a1fb546
andiboshtrakaj <andiboshtraj1@gmail.com>
2024-10-18 17:00:29 +0200

    added variables list

---
+++ Moduli1/lesson1.py
@@ -0,0 +1,32 @@
+from traceback import print_tb
+
+emri = "Andi"
+mosha = 17
+gjinia = "mashkull"
+
+#print(type(mosha))
+
+#mosha = mosha + 10
+
+#print(mosha)
+
+#emri_mbiemri = emri + " " + "Boshtrakaj"
+
+#print(emri_mbiemri)
+
+my_data = ["Andi", "Boshtrakaj", 17, "Shkolla Digjitale"]
+
+mosha = my_data[2]
+
+#print(mosha)
+
+my_data.append("Prishtine")
+
+#print(my_data)
+
+my_data.insert(4,"klasa 12")
+print(my_data)
+
+my_data.remove("Andi")
+del my_data[0]
+print(my_data)
